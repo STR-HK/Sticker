cc79ee70deb9ba027b3fca723801102297504274
STR-HK <78652371+STR-HK@users.noreply.github.com>
2021-08-16 22:48:15 +0900

    Initial commit

---
+++ Sticker.py
@@ -0,0 +1,177 @@
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import sys
+
+import io
+from PIL import Image
+from PIL.ImageQt import ImageQt
+
+import math
+
+import os
+
+
+class Sticker(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowType.WindowStaysOnTopHint)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+
+        layout = QVBoxLayout()
+
+        self.img = QLabel()
+
+        layout.addWidget(self.img)
+
+        self.imgsize = 400
+
+        # sizegrip = QSizeGrip(self)
+        # layout.addWidget(sizegrip, 0, Qt.AlignBottom | Qt.AlignRight)
+        self.setLayout(layout)
+
+        self.show()
+
+        self.setPixmap(
+            os.path.abspath(os.path.join(os.path.dirname(__file__), "67669539.png"))
+        )
+
+    def setPixmap(self, path=None, pixmap=None):
+        if pixmap:
+            self.setGeometry(
+                self.x(),
+                self.y(),
+                self.imgsize * 1,
+                int(self.imgsize * (pixmap.height() / pixmap.width())),
+            )
+
+            self.img.setPixmap(pixmap)
+            return
+
+        try:
+            self.movie.stop()
+        except:
+            print("움짤기록없음")
+        self.image = Image.open(path)
+        self.setGeometry(
+            self.x(),
+            self.y(),
+            self.imgsize * 1,
+            int(self.imgsize * (self.image.height / self.image.width)),
+        )
+        pixmap = QPixmap.fromImage(
+            ImageQt(
+                self.image.resize(
+                    (
+                        self.imgsize * 1,
+                        int(self.imgsize * (self.image.height / self.image.width)),
+                    )
+                )
+            )
+        )
+        self.img.setPixmap(pixmap)
+
+    def setMovie(self, path):
+        self.movie = QMovie(path)
+        self.movie.frameChanged.connect(self.repaint)
+        self.movie.start()
+
+    def repaint(self):
+        self.setPixmap(pixmap=self.movie.currentPixmap())
+
+    def contextMenuEvent(self, event):
+        contextMenu = QMenu(self)
+        # newAct = contextMenu.addAction("New")
+        openAct = contextMenu.addAction("Open")
+        quitAct = contextMenu.addAction("Quit")
+        action = contextMenu.exec_(self.mapToGlobal(event.pos()))
+
+        if action == openAct:
+            dialog = QFileDialog()
+            dialog.setFileMode(QFileDialog.AnyFile)
+
+            filename = dialog.getOpenFileName(
+                caption="Open Image file",
+                filter="Image files (*.bmp *.jpg *.jpeg *.png *.gif)",
+                directory=".",
+            )
+
+            if filename:
+                if filename[0] == "":
+                    return
+
+                if filename[0].split(".")[-1] == "gif":
+                    print("움짤")
+                    self.setMovie(filename[0])
+
+                else:
+                    self.setPixmap(filename[0])
+
+        if action == quitAct:
+            self.close()
+
+    def mouseDoubleClickEvent(self, event):
+        print(event.pos())
+
+    def mousePressEvent(self, event):
+        self.offset = event.pos()
+
+    def mouseMoveEvent(self, event):
+        try:
+            if self.offset:
+                x = event.globalX()
+                y = event.globalY()
+                x_w = self.offset.x()
+                y_w = self.offset.y()
+                self.move(x - x_w, y - y_w)
+        except:
+            print("error")
+
+    # def mouseReleaseEvent(self, event):
+    #     self.offset = None
+    #     screen = QDesktopWidget().availableGeometry()
+    #     widget = self.geometry()
+
+    #     left = widget.left() - screen.left()
+    #     top = widget.top() - screen.top()
+    #     right = screen.right() - widget.right()
+    #     bottom = screen.bottom() - widget.bottom()
+
+    #     offset = 100
+    #     relative_zero = 0
+    #     relative_top = widget.top()
+    #     relative_left = widget.left()
+    #     relative_right = screen.width() - widget.width()
+    #     relative_bottom = screen.height() - widget.height()
+
+    #     if left < offset:
+    #         self.move(relative_zero, relative_top)
+    #         if top < offset:
+    #             self.move(relative_zero, relative_zero)
+    #         elif bottom < offset:
+    #             self.move(relative_zero, relative_bottom)
+    #     elif right < offset:
+    #         self.move(relative_right, relative_top)
+    #         if top < offset:
+    #             self.move(relative_right, relative_zero)
+    #         elif bottom < offset:
+    #             self.move(relative_right, relative_bottom)
+    #     elif top < offset:
+    #         self.move(relative_left, relative_zero)
+    #     elif bottom < offset:
+    #         self.move(relative_left, relative_bottom)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Sticker()
+
+    # palatte = window.palette()
+    # trs = QColor("transparent")
+    # trs.setAlpha(100)
+    # palatte.setColor(window.backgroundRole(), trs)
+    # window.setPalette(palatte)
+
+    window.show()
+    sys.exit(app.exec())
---
+++ sizedoc copy.py
@@ -0,0 +1,80 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class SideGrip(QtWidgets.QWidget):
+    def __init__(self, parent, edge):
+        QtWidgets.QWidget.__init__(self, parent)
+
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton:
+            self.mousePos = event.pos()
+
+    def mouseMoveEvent(self, event):
+        if self.mousePos is not None:
+            delta = event.pos() - self.mousePos
+            self.resizeFunc(delta)
+
+    def mouseReleaseEvent(self, event):
+        self.mousePos = None
+
+
+class Main(QtWidgets.QMainWindow):
+    _gripSize = 8
+
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+
+        # corner grips should be "on top" of everything, otherwise the side grips
+        # will take precedence on mouse events, so we are adding them *after*;
+        # alternatively, widget.raise_() can be used
+        self.cornerGrips = [QtWidgets.QSizeGrip(self) for i in range(4)]
+
+    @property
+    def gripSize(self):
+        return self._gripSize
+
+    def setGripSize(self, size):
+        if size == self._gripSize:
+            return
+        self._gripSize = max(2, size)
+        self.updateGrips()
+
+    def updateGrips(self):
+        self.setContentsMargins(*[self.gripSize] * 4)
+
+        outRect = self.rect()
+        # an "inner" rect used for reference to set the geometries of size grips
+        inRect = outRect.adjusted(
+            self.gripSize, self.gripSize, -self.gripSize, -self.gripSize
+        )
+
+        # bottom right
+        self.cornerGrips[2].setGeometry(
+            QtCore.QRect(inRect.bottomRight(), outRect.bottomRight())
+        )
+
+        # left edge
+        self.sideGrips[0].setGeometry(0, inRect.top(), self.gripSize, inRect.height())
+        # top edge
+        self.sideGrips[1].setGeometry(inRect.left(), 0, inRect.width(), self.gripSize)
+        # right edge
+        self.sideGrips[2].setGeometry(
+            inRect.left() + inRect.width(), inRect.top(), self.gripSize, inRect.height()
+        )
+        # bottom edge
+        self.sideGrips[3].setGeometry(
+            self.gripSize, inRect.top() + inRect.height(), inRect.width(), self.gripSize
+        )
+
+    def resizeEvent(self, event):
+        QtWidgets.QMainWindow.resizeEvent(self, event)
+        self.updateGrips()
+
+
+app = QtWidgets.QApplication([])
+m = Main()
+m.show()
+m.resize(240, 160)
+app.exec_()
